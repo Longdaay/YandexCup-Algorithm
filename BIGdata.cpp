e0341a555da95e6cf0bd211d52b4c1bb446355cf
Сергей <sergolongday@mail.ru>
2020-09-29 20:58:29 +0300

    comment

--- BIGdata.cpp
+++ BIGdata.cpp
@@ -2,169 +2,78 @@
 #include <vector>
 #include <fstream>
 #include <math.h>
-#include <ctime>//
-#include <chrono>//
 
 using namespace std;
-unsigned long long int max_value = 9 * pow(10, 9) + 9;
-
 
 class BigData {
 public:
 	BigData();
-	BigData(char name);
-	vector < int> getVecFile( int& size, ifstream& in);
-	vector<int> getVec(int& Size);
+	vector < int> getVecFile(ifstream& in);
 	void fillSpeadsheet();
 	void findMaxSumm(int i_offset, int j_offset, unsigned long long int curr_summ);
-	void findMaxSummopt(int i_offset, int j_offset, unsigned long long int curr_summ);
 	unsigned long long int getSumm() { return summ; }
-	void setsumm(unsigned long long int summ) { this->summ = summ; }
 private:
-	vector< int> a_vec;
-	vector< int> b_vec;
-	vector<vector<unsigned long long int>> spreadsheet;
-	unsigned long long int summ = 0;
+	vector< int> a_vec;										// 1 массив
+	vector< int> b_vec;									   // 2 массив 
+	vector<vector<unsigned long long int>> spreadsheet;	  // матрица 
+	unsigned long long int summ = 0;					 // максимальная найденная сумма
 };
 
-BigData::BigData() {
-	ifstream in;
-	 int sizeA, sizeB;
-	in.open("input.txt");
-	in >> sizeA;
-	in >> sizeB;
-	a_vec = getVecFile(sizeA, in);
-	b_vec = getVecFile(sizeB, in);
-	in.close();
-	fillSpeadsheet();
+BigData::BigData() {					// конструктор
+	ifstream in;					   // объявляем поток для файла
+	int sizeA, sizeB;				  // переменные для размеров столбца и строки 
+	in.open("input.txt");			 // открываем файл
+	in >> sizeA;					// получем размер 1го массива
+	in >> sizeB;				   // получем размер 2го массива
+	a_vec = getVecFile(in);		  // получаем из файла 1 массив
+	b_vec = getVecFile(in);		 // получаем из файла 2 массив
+	in.close();					// поток закрываем
+	fillSpeadsheet();		   // заполняем матрицу этими массивами
 }
 
-BigData::BigData(char name) {
-	int sizeA, sizeB;
-	cin >> sizeA;
-	cin >> sizeB;
-	a_vec = getVec(sizeA);
-	b_vec = getVec(sizeB);
-	fillSpeadsheet();
+vector <int> BigData::getVecFile(ifstream& in) {	// получаем из файла массив чисел (на вход принимаем поток с файла) на выход отдаем массив чисел из файла
+	vector< int> vec;							   // массив в который запишем наши числа
+	int temp;									  // переменная куда будут записываться числа из файла
+	char t;										 // символьная переменная	
+	in >> temp;									// получаем первое число
+	vec.push_back(temp);					   // записываем его
+	in.get(t);							   	  // считываем следующий символ в файле
+	while (t != '\n') {					 	 // пока эта переменная не получит символ конца строки продолжаем записывать числа в массив
+		if (!in.eof()) {					// если не достигли конца файла - записываем, иначе - символу присваиваем конец строки
+			in >> temp;				 	   //
+			vec.push_back(temp);		  //
+			in.get(t);					 //
+		}								//
+		else					  	   //
+			t = '\n';	 			  //
+	}								 //
+	return vec;						//возвращаем массив
 }
 
-vector<int>BigData::getVec(int& Size) {//
-	vector<int> vec;
-	for (int i = 0; i < Size; i++) {
-		vec.push_back(rand() % 10);
-	}
-	return vec;
+void BigData::fillSpeadsheet() {											// заполняем матрицу
+	spreadsheet.resize(a_vec.size());									   // расширяем строки таблицы на размерность 1го массива 
+	for (int i = 0; i < a_vec.size(); i++) {							  // проходим циклом по строкам
+		spreadsheet[i].resize(b_vec.size());							 // расширяем столбцы таблицы на размерность 2го массива
+		for (int j = 0; j < b_vec.size(); j++) {						// проходим циклом по столбцам
+			spreadsheet[i][j] = a_vec[i] * pow(10,9) + b_vec[j];	   // заполняем элемент матрицы значением по формуле (pow - возведение в степень (10 - число, 9 - степень))
+		}															  //
+	}																 //
 }
 
-vector < int> BigData::getVecFile(int& size, ifstream& in) {
-	vector< int> vec;
-	int temp;
-	char t;
-	vec.clear();
-	in >> temp;
-	vec.push_back(temp);
-	in.get(t);
-	while (t != '\n') {
-		if (!in.eof()) {
-			in >> temp;
-			vec.push_back(temp);
-			in.get(t);
-		}
-		else
-			t = '\n';
-	}
-	return vec;
+void BigData::findMaxSumm(int i_offset, int j_offset, unsigned long long int curr_summ) { // поиск максимальной суммы. Рекурсивная функция. i_offset - номер строки,
+																						 // j_offset - номер столбца, curr_summ - текущая сумма по пройденному пути
+	if (i_offset == a_vec.size())														// если номер строки достиг края, то этот путь закончен, возвращаемся
+		return;																		   //
+	if (j_offset == b_vec.size())													  // если номер столбца достиг края, то этот путь закончен, возвращаемся
+		return;																	     //
+	curr_summ += spreadsheet[i_offset][j_offset];									// добавляем к текущей сумме пройденного пути значение элемента i-строки j-столбца
+	curr_summ > summ ? summ = curr_summ : summ;									   // тренарный оператор. Если текущая сумма пройденного пути больше суммы максимально найденной - присваиваем максимальной текущуюю. Иначе - ничего не делаем 
+	findMaxSumm(i_offset + 1, j_offset, curr_summ);							      // идем снова по функции, опустившись вниз (i + 1)
+	findMaxSumm(i_offset, j_offset + 1, curr_summ);								 // идем снова по функции, пройдя вправо (j + 1)
 }
-
-void BigData::fillSpeadsheet() {
-	spreadsheet.resize(a_vec.size());
-	for (int i = 0; i < a_vec.size(); i++) {
-		spreadsheet[i].resize(b_vec.size());
-		for (int j = 0; j < b_vec.size(); j++) {
-			spreadsheet[i][j] = a_vec[i] * pow(10,9) + b_vec[j];
-		}
-	}
-}
-
-void BigData::findMaxSumm(int i_offset, int j_offset, unsigned long long int curr_summ) {
-	
-	if (i_offset == a_vec.size())
-		return;
-	if (j_offset == b_vec.size())
-		return;
-	
-	/*int max_steps = a_vec.size() + b_vec.size() - 2;
-	int current_steps = i_offset + j_offset - 2;
-	int steps_left = max_steps - current_steps;
-	if (curr_summ + (steps_left * max_value) < summ)
-		return;*/
-	curr_summ += spreadsheet[i_offset][j_offset];
-	curr_summ > summ ? summ = curr_summ : summ;
-	findMaxSumm(i_offset + 1, j_offset, curr_summ);
-	findMaxSumm(i_offset, j_offset + 1, curr_summ);
-}
-
-void BigData::findMaxSummopt(int i_offset, int j_offset, unsigned long long int curr_summ) {
-
-	if (i_offset == a_vec.size())
-		return;
-	if (j_offset == b_vec.size())
-		return;
-
-	int max_steps = a_vec.size() + b_vec.size() - 2;
-	int current_steps = i_offset + j_offset - 2;
-	int steps_left = max_steps - current_steps;
-	if (curr_summ + (steps_left * max_value) < summ)
-		return;
-	curr_summ += spreadsheet[i_offset][j_offset];
-	curr_summ > summ ? summ = curr_summ : summ;
-	findMaxSummopt(i_offset + 1, j_offset, curr_summ);
-	findMaxSummopt(i_offset, j_offset + 1, curr_summ);
-}
-
 int main() {
-	chrono::steady_clock sc;
-	srand(time(0));//
-	BigData bd(' ');//
-	auto start = sc.now();     // устанавливаем начало отсчета времени события
-	bd.findMaxSumm(0, 0, 0);
-	auto end = sc.now();       // устанавливаем конец отсчета времени события
-	auto time_span = static_cast<chrono::duration<double>>(end - start);   // высчитываем время, затраченное на событи
-	cout << bd.getSumm();
-	cout << "\nTime: " << time_span.count() << " second" << endl << endl;
-
-	bd.setsumm(0);
-	auto start1 = sc.now();     // устанавливаем начало отсчета времени события
-	bd.findMaxSumm(0, 0, 0);
-	auto end1 = sc.now();       // устанавливаем конец отсчета времени события
-	auto time_span1 = static_cast<chrono::duration<double>>(end1 - start1);   // высчитываем время, затраченное на событи
-	cout << bd.getSumm();
-	cout << "\nTime optimization: " << time_span1.count() << " second" << endl << endl;
+	BigData bd;						// экземпляр класса
+	bd.findMaxSumm(0, 0, 0);	   // вызываем поиск максимальной суммы, задав начальную позицию 0,0, и текущую сумму 0
+	cout << bd.getSumm();		  // выводим сумму
 	return 0;
 }
-/*
-void BigData::printSheet() {
-	for (auto& token : spreadsheet) {
-		for (auto& elem : token) {
-			cout << elem << " ";
-		}
-		cout << endl;
-	}
-}
-
-void BigData::printVec(vector< int> vec) {
-	for (auto& token : vec)
-		cout << token << " ";
-	cout << endl;
-}
-void BigData::findMaxSumm(int i_offset, int j_offset, unsigned long long int curr_summ) {
-	if (i_offset == a_vec.size())
-		return;
-	if (j_offset == b_vec.size())
-		return;
-	curr_summ += spreadsheet[i_offset][j_offset];
-	curr_summ > summ ? summ = curr_summ : summ;
-	findMaxSumm(i_offset + 1, j_offset, curr_summ);
-	findMaxSumm(i_offset, j_offset + 1, curr_summ);
-}
-*/
