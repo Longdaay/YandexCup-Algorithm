77856b6cc24fcc70e59d81a9ef768cc013e44d8b
Сергей <sergolongday@mail.ru>
2020-10-06 00:08:41 +0300

    input CompsConnections like MergeSheet

--- NetworkTopology.cpp
+++ NetworkTopology.cpp
@@ -9,6 +9,7 @@ public:
 	Topology();
 	void setCompsConnections();
 	void printConnections();
+    void Dijkstra(int st);
 private:
 	int countComps;
 	vector <vector<int>> CompsConnections;
@@ -25,7 +26,9 @@ void Topology::setCompsConnections() {
 	char t;
 	int iter = 0;
 	in >> countComps;
+	CompsConnections.resize(countComps);
 	while (iter < countComps - 1) {
+		CompsConnections[iter].resize(countComps);
 		CompConnection.clear();
 		in >> temp;
 		CompConnection.push_back(temp);
@@ -39,10 +42,56 @@ void Topology::setCompsConnections() {
 			else
 				t = '\n';
 		}
-		CompsConnections.push_back(CompConnection);
+		CompsConnections[CompConnection[0] - 1][CompConnection[1] - 1] = 1;
+		CompsConnections[CompConnection[1] - 1][CompConnection[0] - 1] = 1;
+		
 		iter++;
 	}
 	in.close();
+}
+
+void Topology::Dijkstra(int st)
+{
+    vector<bool> visited;
+	visited.resize(countComps);
+    vector<int> D;
+	D.resize(countComps);
+    for (int i = 0; i < countComps; i++)
+    {
+        D[i] = CompsConnections[st][i];
+        visited[i] = false;
+    }
+    D[st] = 0;
+    int index = 0, u = 0;
+    for (int i = 0; i < countComps; i++)
+    {
+        int min = INT_MAX;
+        for (int j = 0; j < countComps; j++)
+        {
+            if (!visited[j] && D[j] < min)
+            {
+                min = D[j];
+                index = j;
+            }
+        }
+        u = index;
+        visited[u] = true;
+        for (int j = 0; j < countComps; j++)
+        {
+            if (!visited[j] && CompsConnections[u][j] != INT_MAX && D[u] != INT_MAX && (D[u] + CompsConnections[u][j] < D[j]))
+            {
+                D[j] = D[u] + CompsConnections[u][j];
+            }
+        }
+    }
+    cout << "Стоимость пути из начальной вершины до остальных(Алгоритм Дейкстры):\t\n";
+    for (int i = 0; i < countComps; i++)
+    {
+        if (D[i] != INT_MAX)
+            cout << st << " -> " << i << " = " << D[i] << endl;
+        else
+            cout << st << " -> " << i << " = " << "маршрут недоступен" << endl;
+    }
 }
 
 void Topology::printConnections() {
@@ -56,5 +105,7 @@ void Topology::printConnections() {
 int main() {
 	Topology tp;
 	tp.printConnections();
+	cout << endl;
+	//tp.Dijkstra(0);
 	return 0;
 }
